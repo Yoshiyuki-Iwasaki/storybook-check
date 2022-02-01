f969fd1a84ed5dc096bed319ed6e797f8fa518b1
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-02 01:41:54 +0900

    add global styled in storybook.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import GlobalStyle from "../src/components/particles/globalStyles";
+import { ThemeProvider } from "styled-components";
+import themeDefault from "../src/components/particles/themeDefault";
+/*
+ * Global decorator to apply the styles to all stories
+ * Read more about them at:
+ * https://storybook.js.org/docs/react/writing-stories/decorators#global-decorators
+ */
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={themeDefault}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +23,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
