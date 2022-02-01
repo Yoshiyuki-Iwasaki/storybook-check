f4b43af793ec01355a2c6ead24fedfe4841adb52
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-01 19:19:03 +0900

    create postContent component.

---
+++ src/components/molecules/post/postContent.styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StylesPostContent = styled.article`
+  padding: 64px 0;
+  margin: 0 auto;
+  max-width: 700px;
+`;
+
+export default StylesPostContent;
---
+++ src/components/molecules/post/postContent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ParseHTML from "../../particles/parseHTML";
+import StylesPostContent from "./postContent.styles";
+
+const PostContent = ({ content }: any) => (
+  <StylesPostContent>{ParseHTML(content)}</StylesPostContent>
+);
+
+export default PostContent;
