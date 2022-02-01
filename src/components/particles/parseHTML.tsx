557d401a6176d76a3a3ce35c015014edf96fdc8d
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-01 19:19:20 +0900

    create parceHTML component.

---
+++ src/components/particles/parseHTML.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Parser from "html-react-parser";
+
+const config = {};
+
+const parseHTML = (html: string) => {
+  const clean: any = Parser(html, config);
+  return clean;
+};
+
+export default parseHTML;
