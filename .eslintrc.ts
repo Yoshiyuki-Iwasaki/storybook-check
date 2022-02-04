788b18b85e7ba1dfd550df2dcde5be0dbf74d8d5
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-04 22:42:31 +0900

    fix prettier and eslint.

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -4,10 +4,18 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
     'plugin:react/recommended',
+    // Airbnbが提供する共有設定。広く使われている
+    'airbnb',
+    // 各プラグイン推奨共有設定
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   globals: {
     Atomics: 'readonly',
