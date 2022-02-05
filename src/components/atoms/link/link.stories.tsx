9d17e79030914fbe1e17a72dfc4e772a360fb816
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-05 20:01:18 +0900

    create link component.

---
+++ src/components/atoms/link/link.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import knobData from './link.knobs.json';
+const { innerText } = knobData;
+
+export const basicLink = () => (
+  <a>{text(innerText.label, innerText.default, innerText.group)}</a>
+);
+
+export default {
+  decorators: [withKnobs],
+  title: 'Link',
+};
