d8110fc748c44b375a406b8c44373f37ec832b59
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-07 20:52:48 +0900

    fix banner component.

--- src/components/organisms/banner/banner.stories.tsx
+++ src/components/organisms/banner/banner.stories.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { withKnobs, array } from '@storybook/addon-knobs';
+import { object, select, text } from '@storybook/addon-knobs';
+import { withDesign } from 'storybook-addon-designs';
 
 import Banner from './banner';
 
 import knobData from './banner.knobs.json';
-const { banner }: any = knobData;
+const { content, cta, title, variant }: any = knobData;
 
-export const standardBanner = () => <Banner props={banner} />;
+export const standardBanner = () => (
+  <Banner
+    content={text(content.label, content.default, content.group)}
+    cta={object(cta.label, cta.default, cta.group)}
+    title={text(title.label, title.default, title.group)}
+    variant={select(
+      variant.label,
+      variant.default,
+      variant.options,
+      variant.group
+    )}
+  />
+);
 
 export default {
   component: Banner,
-  decorators: [withKnobs],
+  decorators: [withDesign],
   title: 'Organisms/Banner',
 };
--- src/components/organisms/banner/banner.tsx
+++ src/components/organisms/banner/banner.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-
 import StyledBanner from './banner.styles';
 
 import Button from '../../atoms/button/button';
 
-const Banner = ({ props }) => {
+import ParseHTML from '../../particles/parseHTML';
+
+const Banner = ({ content, cta, title }: any) => {
   return (
     <StyledBanner>
       <div className="banner__inner">
-        {props.title && <h2 className="banner__title">{props.title}</h2>}
-        {props.text && <p className="banner__text">{props.text}</p>}
-        {props.button[0] && (
-          <Button href={props.button[0].url}>{props.button[0].text}</Button>
+        {title && <h2 className="banner__title">{title}</h2>}
+        {content && <div className="banner__text">{ParseHTML(content)}</div>}
+        {cta && cta.title && cta.title !== '' && (
+          <footer className="banner__footer">
+            <Button href={cta.url}>{cta.title}</Button>
+          </footer>
         )}
       </div>
     </StyledBanner>
