262bbe1117a81559b522a8498c084373425e1574
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-07 20:53:05 +0900

    fix template/post component.

--- src/components/templates/posts/post.tsx
+++ src/components/templates/posts/post.tsx
@@ -3,11 +3,19 @@ import React from 'react';
 import StyledPost from './post.styles';
 
 import PostContent from '../../molecules/post/postContent';
+import Header from '../../organisms/header/header';
+import Footer from '../../organisms/footer/footer';
+import Banner from '../../organisms/banner/banner';
+import Related from '../../organisms/related/related';
 
-const PostTemplate = ({ content }: any) => {
+const PostTemplate = ({ banner, content, footer, header, related }: any) => {
   return (
     <StyledPost>
+      <Header {...header} />
       <PostContent content={content} />
+      <Banner {...banner} />
+      <Related {...related} />
+      <Footer {...footer} />
     </StyledPost>
   );
 };
